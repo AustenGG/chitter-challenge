b9a1f7b9778b9ae5ee39df49bffd3a92c1afb460
Austen Grice-Griffin <austen.grice-griffin@Austens-MacBook-Pro.local>
2018-10-29 14:25:41 +0000

    rspec{

--- spec/features/adding_name_spec.rb
+++
@@ -1,9 +0,0 @@
-feature 'adding user name' do
-  scenario 'filling in name' do
-    visit '/'
-    fill_in('name', with: 'Jhon')
-    click_button('Submit')
-
-    expect(page).to have_content('Jhon')
-  end
-end
--- spec/features/adding_post_spec.rb
+++ spec/features/adding_post_spec.rb
@@ -1,10 +1,3 @@
-feature 'Testing Chitter' do
-  scenario 'Chitter heading' do
-    visit '/'
-    fill_in('peep', with:'hey my first peep!')
-    expect(page).to have_content('hey my first peep!')
-  end
-end
 
 feature 'Testing Username field' do
   scenario 'Username field' do
