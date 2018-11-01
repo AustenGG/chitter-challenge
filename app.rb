ad5d40ca823d50e7b51ce239404f3f215f66b083
Ahmed Marzook <m4marzook@gmail.com>
2018-11-01 13:19:44 +0000

    App fix

--- app.rb
+++ app.rb
@@ -4,13 +4,13 @@ class Chitter < Sinatra::Base
   get '/' do
     erb :index
   end
-  
+
   post '/feed' do
     session[:username] = params[:username]
     session[:peep] = params[:peep]
     erb :feed
   end
-  get '/' do
+  get '/sign-up' do
     erb :sign_up
   end
 
